baffa4de12ce057c7aacf22c3c699774ae64e484
eduardoyuki <yukieduardo1@gmail.com>
2024-05-29 19:25:26 -0300

    Mensagem do commit aqui

--- apps/web/src/app/(home)/page.tsx
+++ apps/web/src/app/(home)/page.tsx
@@ -3,7 +3,6 @@ import { VideosSection } from "./_components/videosSection";
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <div>HEADER</div>
       <VideosSection />
     </div>
   );
